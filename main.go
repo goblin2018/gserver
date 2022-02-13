aa2020d81e7604ec6501c0c7eabca15969f5d706
root <root@goblin.localdomain>
2022-02-13 23:46:51 +0800

    添加简单的示例及Dockerfile

--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"gserver/routers"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	fmt.Print("Hello Gserver")
 
-	server := &http.Server{
-		Addr: "",
-		Handler: ,
-		
+	s := &http.Server{
+		Addr:           ":9000",
+		Handler:        routers.InitRoute(),
+		ReadTimeout:    time.Second * 60,
+		WriteTimeout:   time.Second * 60,
+		MaxHeaderBytes: 1 << 20,
 	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			panic(err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+	log.Println("Server exiting")
+
 }
--- routers/router.go
+++ routers/router.go
@@ -1,9 +1,21 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitRoute() *gin.Engine {
 	r := gin.Default()
 	gin.SetMode(gin.DebugMode)
+
+	r.GET("", getUser)
 	return r
+}
+
+func getUser(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "hello me",
+	})
 }
